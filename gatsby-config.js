f65a0f30d808ea540c22421a9974ed50616afc3f
Mateusz_Madej <m.madej@emakina.pl>
2022-11-09 10:36:26 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Test Page 1`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms"]
+};
